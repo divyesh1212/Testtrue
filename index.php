c194172a6ceb75c78657b5c8eaf8cc92f4add8f0
AlpeshT <alpesh.trivedi.1981@gmail.com>
2015-06-13 11:37:40 +0530

    no message

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/xampp/');
+echo "hello";
 	exit;
 ?>
-Something is wrong with the XAMPP installation :-(
